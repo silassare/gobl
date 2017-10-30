b1d22c7db73fa39266c0fe3b1dc639800b8f10ba
Emile Silas SARE <emile.silas@gmail.com>
2017-10-30 18:27:58 +0100

    relations between tables added

---
+++ src/DBAL/Relations/ManyToMany.php
@@ -0,0 +1,21 @@
+<?php
+	/**
+	 * Copyright (c) Emile Silas Sare <emile.silas@gmail.com>
+	 *
+	 * This file is part of the Gobl package.
+	 *
+	 * For the full copyright and license information, please view the LICENSE
+	 * file that was distributed with this source code.
+	 */
+
+	namespace Gobl\DBAL\Relations;
+
+	use Gobl\DBAL\Table;
+
+	class ManyToMany extends Relation
+	{
+		public function __construct($name, Table $host_table, Table $target_table, array $columns = null)
+		{
+			parent::__construct($name, $host_table, $target_table, $columns, Relation::MANY_TO_MANY);
+		}
+	}
---
+++ src/DBAL/Relations/Relation.php
@@ -0,0 +1,145 @@
+<?php
+	/**
+	 * Copyright (c) Emile Silas Sare <emile.silas@gmail.com>
+	 *
+	 * This file is part of the Gobl package.
+	 *
+	 * For the full copyright and license information, please view the LICENSE
+	 * file that was distributed with this source code.
+	 */
+
+	namespace Gobl\DBAL\Relations;
+
+	use Gobl\DBAL\Exceptions\DBALException;
+	use Gobl\DBAL\Table;
+
+	abstract class Relation
+	{
+		const ONE_TO_ONE   = 1;
+		const ONE_TO_MANY  = 2;
+		const MANY_TO_ONE  = 3;
+		const MANY_TO_MANY = 4;
+
+		/** @var int */
+		protected $type;
+		/** @var \Gobl\DBAL\Table */
+		protected $host_table;
+		/** @var \Gobl\DBAL\Table */
+		protected $target_table;
+		/** @var bool */
+		protected $target_is_slave;
+		/**@var array */
+		protected $relation_columns;
+		/** @var string */
+		protected $name;
+
+		public function __construct($name, Table $host_table, Table $target_table, array $columns = null, $type)
+		{
+			// the relation is based on foreign keys
+			if (is_null($columns)) {
+				// the slave table contains foreign key from the master table
+				if ($target_table->hasForeignKeyConstraint($host_table)) {
+					$this->target_is_slave = true;
+					$columns               = $target_table->getForeignKeyConstraintFrom($host_table)
+														  ->getConstraintColumns();
+				} elseif ($host_table->hasForeignKeyConstraint($target_table)) {
+					$this->target_is_slave = false;
+					$columns               = $host_table->getForeignKeyConstraintFrom($target_table)
+														->getConstraintColumns();
+				} else {
+					throw new DBALException(sprintf('Error in relation "%s", there is no columns to link the table "%s" to the table "%s".', $name, $host_table->getName(), $target_table->getName()));
+				}
+			} else {
+				$cols = [];
+				foreach ($columns as $from_column => $target_column) {
+					$host_table->assertHasColumn($from_column);
+					$target_table->assertHasColumn($target_column);
+					$from_column   = $host_table->getColumn($from_column)
+												->getFullName();
+					$target_column = $target_table->getColumn($target_column)
+												  ->getFullName();
+
+					$cols[$from_column] = $target_column;
+				}
+
+				$this->target_is_slave = true;
+				$columns               = $cols;
+			}
+
+			$this->name             = $name;
+			$this->type             = $type;
+			$this->host_table       = $host_table;
+			$this->target_table     = $target_table;
+			$this->relation_columns = $columns;
+		}
+
+		/**
+		 * Gets the relation host table.
+		 *
+		 * @return \Gobl\DBAL\Table
+		 */
+		public function getHostTable()
+		{
+			return $this->host_table;
+		}
+
+		/**
+		 * Gets the relation target table.
+		 *
+		 * @return \Gobl\DBAL\Table
+		 */
+		public function getTargetTable()
+		{
+			return $this->target_table;
+		}
+
+		/**
+		 * Gets the relation master table.
+		 *
+		 * @return \Gobl\DBAL\Table
+		 */
+		public function getMasterTable()
+		{
+			return $this->target_is_slave ? $this->host_table : $this->target_table;
+		}
+
+		/**
+		 * Gets the relation slave table.
+		 *
+		 * @return \Gobl\DBAL\Table
+		 */
+		public function getSlaveTable()
+		{
+			return $this->target_is_slave ? $this->target_table : $this->host_table;
+		}
+
+		/**
+		 * Gets relation columns.
+		 *
+		 * @return array
+		 */
+		public function getRelationColumns()
+		{
+			return $this->relation_columns;
+		}
+
+		/**
+		 * Gets the relation type.
+		 *
+		 * @return int
+		 */
+		public function getType()
+		{
+			return $this->type;
+		}
+
+		/**
+		 * Gets the relation name.
+		 *
+		 * @return string
+		 */
+		public function getName()
+		{
+			return $this->name;
+		}
+	}
