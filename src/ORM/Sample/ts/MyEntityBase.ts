7210c4bc075edc6747d95fe872c4da6f39554976
Emile Silas SARE <emile.silas@gmail.com>
2020-08-20 20:37:10 +0100

    TS EntityBase class added

---
+++ src/ORM/Sample/ts/MyEntityBase.ts
@@ -0,0 +1,12 @@
+//__GOBL_HEAD_COMMENT__
+import {
+	GoblEntity,
+	_bool,
+	_int,
+	_string,
+} from 'gobl-utils-ts';
+
+export default abstract class MyEntityBase extends GoblEntity {
+	//__GOBL_TS_COLUMNS_CONST__
+//__GOBL_TS_COLUMNS_GETTERS_SETTERS__
+}
