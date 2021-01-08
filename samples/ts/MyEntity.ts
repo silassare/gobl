41af1d3be856bf8906f198c991ddb1fe7cfdbeee
Emile Silas SARE <emile.silas@gmail.com>
2021-01-08 14:46:36 +0100

    GoblEntityData instead of tGoblEntityData

--- samples/ts/MyEntity.ts
+++ samples/ts/MyEntity.ts
@@ -1,18 +1,14 @@
 //__GOBL_HEAD_COMMENT__
-import {
-	getEntityCache,
-	tGoblEntityData,
-} from 'gobl-utils-ts';
-import MyEntityBase from './base/MyEntityBase';
+import { getEntityCache, GoblEntityData } from "gobl-utils-ts";
+import MyEntityBase from "./base/MyEntityBase";
 
 export default class MyEntity extends MyEntityBase {
-
-	constructor(data?: tGoblEntityData) {
-		super(data, 'MyEntity', MyEntity.PREFIX, MyEntity.COLUMNS);
+	constructor(data?: GoblEntityData) {
+		super(data, "MyEntity", MyEntity.PREFIX, MyEntity.COLUMNS);
 	}
 
 	public static fromCache(cacheKey: string): MyEntity | undefined {
-		const cache: any = getEntityCache('MyEntity');
+		const cache: any = getEntityCache("MyEntity");
 		return cache && cache[cacheKey];
 	}
 }
