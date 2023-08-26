40100a2a5539e2c8481cae20b5a5f7bf2291fa20
Emile Silas SARE <emile.silas@gmail.com>
2023-08-26 14:49:28 +0100

    add insert multiple capabilities

--- src/DBAL/Queries/QBInsert.php
+++ src/DBAL/Queries/QBInsert.php
@@ -16,8 +16,9 @@ namespace Gobl\DBAL\Queries;
 use Gobl\DBAL\Interfaces\RDBMSInterface;
 use Gobl\DBAL\Queries\Interfaces\QBInterface;
 use Gobl\DBAL\Queries\Traits\QBCommonTrait;
-use Gobl\DBAL\Queries\Traits\QBSetColumnsTrait;
+use Gobl\DBAL\Queries\Traits\QBSetColumnsValuesTrait;
 use Gobl\DBAL\Table;
+use InvalidArgumentException;
 
 /**
  * Class QBInsert.
@@ -25,9 +26,21 @@ use Gobl\DBAL\Table;
 class QBInsert implements QBInterface
 {
 	use QBCommonTrait;
-	use QBSetColumnsTrait;
+	use QBSetColumnsValuesTrait;
 
 	protected ?string $options_table = null;
+
+	/**
+	 * @var string[]
+	 */
+	protected array $options_columns_names = [];
+
+	/**
+	 * @var array<int, string[]>
+	 */
+	protected array $options_values_params = [];
+
+	protected bool $auto_prefix = true;
 
 	/**
 	 * QBInsert constructor.
@@ -61,26 +74,78 @@ class QBInsert implements QBInterface
 	}
 
 	/**
+	 * Specify the table to insert into.
+	 *
 	 * @param \Gobl\DBAL\Table|string $table
-	 * @param array                   $columns_values_map
 	 * @param bool                    $auto_prefix
 	 *
 	 * @return $this
 	 */
-	public function into(string|Table $table, array $columns_values_map = [], bool $auto_prefix = true): self
+	public function into(string|Table $table, bool $auto_prefix = true): self
 	{
 		$table_name          = $this->resolveTable($table)
 			?->getFullName() ?? $table;
 		$this->options_table = $table_name;
+		$this->auto_prefix   = $auto_prefix;
 
-		return $this->setInsertOrUpdateColumnsValues($table_name, $columns_values_map, $auto_prefix);
+		return $this;
 	}
 
 	/**
+	 * Specify values to insert.
+	 *
+	 * @param array<string, mixed> $values the column => value map
+	 *
+	 * @return $this
+	 */
+	public function values(array $values): self
+	{
+		$map           = $this->bindColumnsValuesForInsertOrUpdate($this->options_table, $values, $this->auto_prefix);
+		$columns       = \array_keys($map);
+		$values_params = \array_values($map);
+
+		if (empty($this->options_columns_names)) {
+			$this->options_columns_names = $columns;
+		} elseif ($this->options_columns_names !== $columns) {
+			throw new InvalidArgumentException(\sprintf(
+				'Invalid columns for multi insert, expected "(%s)" got "(%s)"',
+				\implode(', ', $this->options_columns_names),
+				\implode(', ', $columns)
+			));
+		}
+
+		$this->options_values_params[] = $values_params;
+
+		return $this;
+	}
+
+	/**
+	 * Gets the table to insert into.
+	 *
 	 * @return null|string
 	 */
 	public function getOptionsTable(): ?string
 	{
 		return $this->options_table;
+	}
+
+	/**
+	 * Gets the inserted columns names.
+	 *
+	 * @return string[]
+	 */
+	public function getOptionsColumnsNames(): array
+	{
+		return $this->options_columns_names;
+	}
+
+	/**
+	 * Gets the inserted values params.
+	 *
+	 * @return array<int, string[]>
+	 */
+	public function getOptionsValuesParams(): array
+	{
+		return $this->options_values_params;
 	}
 }
